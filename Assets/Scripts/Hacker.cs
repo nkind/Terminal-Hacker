84bf5480f1dcf40e3b9b1496e6a90fd43c4258f5
Nick Kindrachuk <nkindrachuk@gmail.com>
2019-11-29 17:01:05 -0600

    Menu Screen
    Displays options and can recognize input.

---
+++ Assets/Scripts/Hacker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    void Start()
+    {
+        ShowMainMenu();
+        
+    }
+
+    void ShowMainMenu()
+    {
+        Terminal.WriteLine("Totally Accurate Hacking Simulation\n" +
+            "---------------------------------------\n" +
+            "Pick your poison:\n" +
+            "\n1. Coffee Shop\n" +
+            "2. School\n" +
+            "3. Airport\n" +
+            "\nEnter a number \n" +
+            "(this is totally how hackers do it): ");
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            Terminal.ClearScreen();
+            ShowMainMenu();
+        }
+        else if (input == "beans")
+        {
+            Terminal.WriteLine("How dare you...");
+        }
+        else
+        {
+            Terminal.WriteLine("Invalid input!");
+        }
+    }
+
+}
